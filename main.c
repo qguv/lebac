f264b595ea5729c26b9059d0aa1ee1bbec2b7c99
Quint Guvernator <quint@guvernator.net>
2018-05-28 17:28:29 -0400

    note column

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
-/*
-   mpg123_to_wav.c
-
-   copyright 2007-2016 by the mpg123 project - free software under the terms of the LGPL 2.1
-   see COPYING and AUTHORS files in distribution or http://mpg123.org
-   initially written by Nicholas Humfrey
-
-   The most complicated part is about the choices to make about output format,
-   and prepare for the unlikely case a bastard mp3 might file change it.
-   */
+/* le bac: the badge audio composer
+ * all bugs are qguv's fault
+ * requires libout123 (mpg123) and termbox (included) */
 
 #include <out123.h>
+
 #include <stdio.h>
 #include <strings.h>
 #include <stdint.h>
@@ -17,7 +11,20 @@
 #include <termbox.h>
 #include <unistd.h>
 
-out123_handle *ao = NULL;
+/* set only if your audio driver needs tweaking */
+#define DRIVER NULL
+#define ENCODING "s16";
+
+/* badge audio rate */
+#define RATE 38000L
+
+struct line_t {
+    char note;
+};
+
+struct line_t pattern[16];
+
+int current_line = 0;
 
 void die(const char * const s)
 {
@@ -40,76 +47,64 @@ void tb_puts(int x, int y, const char *s)
 
 int audio(void)
 {
-    char *infile = NULL;
+    out123_handle *ao = NULL;
 
-    const char *encname = "s16";
-    const long rate = 38000;
+    /* s16 since it's the cheapest, but we're really only using INT16_MAX, 0,
+     * and INT16_MIN */
+    const char *encname = ENCODING
     const int channels = 1;
 
-    int volshift = 3;
+    int mercy = 3;
 
-    /* encoding-specific buffer */
+    char *driver = DRIVER;
+
     int16_t *buffer = NULL;
-    const size_t buffer_size = 38000;
 
-    /* try changing your default audio drivers and devices before you fuck
-     * around with these -- you shouldn't have to unless your system is really
-     * exotic. */
-    char *driver = NULL;
-    char *outfile = NULL;
-
-    printf("Input file: %s\n", infile);
-    printf("Output driver: %s\n", driver ? driver : "<nil> (default)");
-    printf("Output file: %s\n", outfile ? outfile : "<nil> (default)");
-
+    /* initialize out123 */
     ao = out123_new();
     if (!ao) {
-        fprintf(stderr, "Cannot create output handle.\n");
+        fprintf(stderr, "out123_new() died, is audio set up on this system?\n");
         out123_del(ao);
-        return -1;
+        return 1;
     }
 
-    /* If that is zero, you'll get the error soon enough from mpg123. */
-    int encoding = out123_enc_byname(encname);
-    if (!encoding) {
-        fprintf(stderr, "No such encoding %s!\n", encname);
+    /* check that the encoding is supported */
+    int encid = out123_enc_byname(encname);
+    if (!encid) {
+        fprintf(stderr, "encoding %s not supported by the default audio driver on this system. choose a better one!\n", encname);
         out123_del(ao);
-        return -1;
+        return 1;
     }
 
-    int err = out123_open(ao, driver, outfile);
+    int err = out123_open(ao, driver, NULL);
     if (err) {
-        fprintf(stderr, "Trouble with out123: %s\n", out123_strerror(ao));
+        fprintf(stderr, "out123_open() died, saying \"%s\"\n", out123_strerror(ao));
         out123_del(ao);
-        return -1;
+        return 1;
     }
 
-    /* It makes no sense for that to give an error now. */
-    out123_driver_info(ao, &driver, &outfile);
+    out123_driver_info(ao, &driver, NULL);
     printf("Effective output driver: %s\n", driver ? driver : "<nil> (default)");
-    printf("Effective output file: %s\n", outfile ? outfile : "<nil> (default)");
-
-    encname = out123_enc_name(encoding);
-    printf( "Playing with %i channels and %li Hz, encoding %s.\n", channels, rate, encname ? encname : "???" );
+    printf("Playing with %i channels and %li Hz, encoding %s.\n", channels, RATE, encname ? encname : "???");
 
     int framesize;
-    if (out123_start(ao, rate, channels, encoding) || out123_getformat(ao, NULL, NULL, NULL, &framesize)) {
+    if (out123_start(ao, RATE, channels, encid) || out123_getformat(ao, NULL, NULL, NULL, &framesize)) {
         fprintf(stderr, "Cannot start output / get framesize: %s\n", out123_strerror(ao));
         out123_del(ao);
-        return -1;
+        return 1;
     }
     fprintf(stderr, "Framesize is %d\n", framesize);
 
-    buffer = malloc(buffer_size);
+    buffer = malloc(RATE);
 
     off_t samples = 0;
     do {
-        long wavelength = rate / 440;
+        long wavelength = RATE / 440;
         long duty = wavelength >> 1;
         for (int i = 0; i < duty; i++)
-            buffer[i] = INT16_MAX >> volshift;
+            buffer[i] = INT16_MAX >> mercy;
         for (int i = duty; i < wavelength; i++)
-            buffer[i] = INT16_MIN >> volshift;
+            buffer[i] = INT16_MIN >> mercy;
 
         size_t bytes_to_play = wavelength * framesize;
         size_t played = out123_play(ao, buffer, bytes_to_play);
@@ -119,13 +114,72 @@ int audio(void)
         }
 
         samples += played / framesize;
-    } while (samples < rate * 5);
+    } while (samples < RATE * 5);
 
     free(buffer);
 
     printf("%li samples written.\n", (long) samples);
     out123_del(ao);
     return 0;
+}
+
+void deconstruct_note(const struct line_t * const line, char * const note_name, char * const accidental, char * const octave)
+{
+    if (line->note == 0) {
+        *note_name = *accidental = *octave = '-';
+        return;
+    }
+
+    if (line->note > 63) {
+        *note_name = *accidental = *octave = '!';
+        return;
+    }
+
+    *note_name  = "bccddeffggaa"[line->note % 12];
+    *accidental = "  # #  # # #"[line->note % 12];
+    *octave = "1234567"[line->note / 12];
+}
+
+void draw_note_column(void)
+{
+    struct tb_cell bright;
+    bright.fg = TB_BLACK;
+    bright.bg = TB_CYAN;
+
+    struct tb_cell dark;
+    dark.fg = TB_DEFAULT;
+    dark.bg = TB_BLACK;
+
+    struct tb_cell black;
+    black.fg = TB_DEFAULT;
+    black.bg = TB_DEFAULT;
+
+    char note_name, accidental, octave;
+    for (int row = 0; row < 16; row++) {
+
+        deconstruct_note(&pattern[row], &note_name, &accidental, &octave);
+
+        if (row % 4 == 0) {
+            bright.ch = note_name;
+            tb_put_cell(3, row + 3, &bright);
+            bright.ch = accidental;
+            tb_put_cell(4, row + 3, &bright);
+            bright.ch = octave;
+            tb_put_cell(5, row + 3, &bright);
+        } else {
+            dark.ch = note_name;
+            tb_put_cell(3, row + 3, &dark);
+            dark.ch = accidental;
+            tb_put_cell(4, row + 3, &dark);
+            dark.ch = octave;
+            tb_put_cell(5, row + 3, &dark);
+        }
+
+        black.ch = (row == current_line) ? '-' : ' ';
+        tb_put_cell(0, row + 3, &black);
+        black.ch = (row == current_line) ? '>' : ' ';
+        tb_put_cell(1, row + 3, &black);
+    }
 }
 
 int main(void)
@@ -140,8 +194,12 @@ int main(void)
         die("Too small--we need at least 80x24\n");
     }
 
-    int line = 0, col = 0;
+    char last_edit = 25;
 
+full_redraw:
+    tb_clear();
+    tb_puts(3, 1, "le bac: the badge audio composer");
+    draw_note_column();
     tb_present();
 
     for (;;) {
@@ -153,11 +211,18 @@ int main(void)
             die("event error\n");
         }
 
-        if (event.type != TB_EVENT_KEY)
+        if (event.type == TB_EVENT_RESIZE)
+            goto full_redraw;
+
+        if (event.type == TB_EVENT_MOUSE)
             continue;
+
+        char this_note;
 
         if (!event.ch) {
             switch (event.key) {
+
+            /* aliases for characters */
             case TB_KEY_ESC:
             case TB_KEY_CTRL_C:
                 event.ch = 'q';
@@ -174,9 +239,27 @@ int main(void)
             case TB_KEY_ARROW_RIGHT:
                 event.ch = 'l';
                 break;
+
             case TB_KEY_ENTER:
                 if (!fork())
                     exit(audio());
+                break;
+
+            /* toggle between this note and no note */
+            case TB_KEY_SPACE:
+                this_note = pattern[current_line].note;
+
+                if (this_note == 0) {
+                    pattern[current_line].note = last_edit;
+                    break;
+                }
+
+                last_edit = pattern[current_line].note;
+                /* fallthrough */
+
+            case TB_KEY_BACKSPACE2:
+            case TB_KEY_DELETE:
+                pattern[current_line].note = 0;
                 break;
             }
         }
@@ -187,25 +270,33 @@ int main(void)
             tb_shutdown();
             return 0;
         case 'h':
-            col--;
-            if (col < 0)
-                col = 0;
+            this_note = pattern[current_line].note;
+            if (this_note == 0)
+                pattern[current_line].note = last_edit;
+            else if (this_note > 1)
+                last_edit = --pattern[current_line].note;
             break;
         case 'j':
-            line++;
+            current_line++;
             break;
         case 'k':
-            line--;
-            if (line < 0)
-                line = 0;
+            current_line--;
             break;
         case 'l':
-            col++;
+            this_note = pattern[current_line].note;
+            if (this_note == 0)
+                pattern[current_line].note = last_edit;
+            else if (this_note < 63)
+                last_edit = ++pattern[current_line].note;
             break;
         }
 
+            if (current_line > 15)
+                current_line = 0;
+            else if (current_line < 0)
+                current_line = 15;
 
-        tb_puts(col, line, "le bac: the badge audio composer");
+        draw_note_column();
         tb_present();
     }
 }
