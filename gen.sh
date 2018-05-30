d9b419c11d10f395fc3d9d0a0115d79daf384803
Quint Guvernator <quint@guvernator.net>
2018-05-30 19:32:02 -0400

    project restructure

--- gen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-out123 -O out.raw --encoding s16 --rate 38000 --timelimit 38000 --wave-freq 440 --wave-pat square
--- run.sh
+++ run.sh
@@ -2,17 +2,19 @@
 (
     cd termbox
     echo "configuring termbox"
-    ./waf configure --prefix=.
+    ./waf configure --out=../build/termbox
     echo "building termbox"
     ./waf
 )
+
 CC="gcc"
 ERROR_OPTS="-Wall -Wpedantic -Wextra -Werror"
-LIBS="-Itermbox/src -Ltermbox/build/src -ltermbox"
-OUTPUT="/tmp/bac"
+LIBS="-Itermbox/src -Lbuild/termbox/src -ltermbox"
+OUTPUT="build/lebac"
+
 echo "removing old bin"
 rm -f $OUTPUT && \
 echo "building le bac" && \
-$CC $LIBS $ERROR_OPTS -o /tmp/lebac main.c && \
+$CC $LIBS $ERROR_OPTS -o "$OUTPUT" src/main.c && \
 echo "running le bac" && \
-DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:termbox/build/src LD_LIBRARY_PATH=$LD_LIBRARY_PATH:termbox/build/src /tmp/lebac "$@"
+DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:build/termbox/src LD_LIBRARY_PATH=$LD_LIBRARY_PATH:build/termbox/src "$OUTPUT" "$@"
