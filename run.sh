657fdddf696ecbb2014e26c299de5c939b8a6889
Quint Guvernator <quint@guvernator.net>
2018-05-29 23:28:36 -0400

    support darwin

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 (
     cd termbox
+    echo "configuring termbox"
     ./waf configure --prefix=.
+    echo "building termbox"
     ./waf
 )
-CC="ccache gcc"
+CC="gcc"
 ERROR_OPTS="-Wall -Wpedantic -Wextra -Werror"
 LIBS="-Itermbox/src -Ltermbox/build/src -ltermbox"
 OUTPUT="/tmp/bac"
-rm -f $OUTPUT && $CC $LIBS $ERROR_OPTS -o /tmp/bac main.c && LD_LIBRARY_PATH=$LD_LIBRARY_PATH:termbox/build/src /tmp/bac "$@"
+echo "removing old bin"
+rm -f $OUTPUT && \
+echo "building le bac" && \
+$CC $LIBS $ERROR_OPTS -o /tmp/lebac main.c && \
+echo "running le bac" && \
+DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:termbox/build/src LD_LIBRARY_PATH=$LD_LIBRARY_PATH:termbox/build/src /tmp/lebac "$@"
