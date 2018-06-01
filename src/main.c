7d66ea4a7a807aa101d9e1b36b17ffb50730e915
Quint Guvernator <quint@guvernator.net>
2018-06-01 17:19:17 -0400

    restructure pattern with duty; it compiles at least

--- src/main.c
+++ src/main.c
@@ -10,7 +10,6 @@
 #include <stdint.h> // int16_t
 #include <stdio.h> // snprintf, fputs (errors)
 #include <stdlib.h> // exit
-//#include <strings.h>
 #include <time.h> // clock_gettime
 #include <unistd.h> // pipe, fork, close, dup, exec, ...
 
@@ -31,22 +30,42 @@
 #define TEMPO_MIN 16
 #define TEMPO_JUMP 10
 
+#define NOTE_MAX 97
+
 #define QUANTIZE(X, MINVAL, MAXVAL) (\
     ((X) == 0) ? 0 : \
     ((X) > 0) ? (MAXVAL) : \
     (MINVAL) \
 )
 
-struct line_t {
-    char note;
-    char lpnote; /* low-priority note */
+const char * const duties[] = {
+    "!!",
+    "50",
+    "25",
+    "12",
+    " 6",
+    " 3",
+    " 1",
+    "L1",
+    "L2",
+    "L3",
+    "L4",
+    "L5",
+    "L6",
+    "L7",
+    "L8",
 };
 
-enum column_t { NOTE, LPNOTE };
+struct note_t {
+    char note;
+    char duty;
+};
+
+enum column_t { LEFT, RIGHT };
 enum global_mode_t { SEQUENCER, HELP };
 enum redraw { NORMAL, FULL };
 
-struct line_t pattern[16];
+struct note_t pattern[16][2];
 int current_line = 0;
 
 unsigned char tempo = 128;
@@ -60,7 +79,7 @@ void die(const char * const s)
     exit(1);
 }
 
-void tb_puts(const char *s, struct tb_cell *cell, int x, int y)
+void tb_puts(const char * const s, struct tb_cell *cell, int x, int y)
 {
     char c;
     for (int i = 0; (c = s[i]); i++) {
@@ -69,7 +88,7 @@ void tb_puts(const char *s, struct tb_cell *cell, int x, int y)
     }
 }
 
-void debug(const char *s)
+void debug(const char * const s)
 {
     static int y = 3;
 
@@ -78,7 +97,7 @@ void debug(const char *s)
         y = 3;
 }
 
-int audio_child(int *pid_p)
+int audio_child(int * const pid_p)
 {
     /* set up pipes to communicate with audio child */
     int pipefds[2];
@@ -111,48 +130,137 @@ int samples_per_cycle(char note)
     return (double) RATE / freq + 0.5L;
 }
 
+/* call audio with the output of audio_child to play audio. you need a new
+ * audio_child pipe each time. we will close the audio pipe for you when it's
+ * done playing.
+ *
+ * the synthesizer alg creates triangle wave approximations with varying duty
+ * cycles. the waves look like this:
+ *
+ *
+ * 50% duty:
+ *
+ *   ,________,                          ,________,
+ *   |        |                          |        |
+ * __|        |________,        ,________|        |________,        ,___...
+ *                     |        |                          |        |
+ *                     |________|                          |________|
+ *
+ *
+ *   |-----------------------------------|
+ *                one wavelength
+ *
+ * -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+ *
+ *  12% duty:
+ *
+ *   ,___,                               ,___,
+ *   |   |                               |   |
+ * __|   |_____________,   ,_____________|   |_____________,   ,________...
+ *                     |   |                               |   |
+ *                     |___|                               |___|
+ *
+ *
+ *   |-----------------------------------|
+ *                one wavelength
+ *
+ * -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+ *
+ * there are two such waves which are added to produce a result--this is what
+ * is meant by "two-channel audio".
+ *
+ * if EMULATE_SHITTY_BADGE_AUDIO is enabled, we'll then restrict the wave to
+ * three possible values: INT16_MAX, 0, and INT16_MIN, representing the push/1,
+ * neutralize/0, and pull/-1 states the H-bridge can take when driving the
+ * buzzer on the badge. this gives you a very accurate simulation of how your
+ * composition will sound when playing on the badge.
+ */
 void audio(int audio_pipe)
 {
-    int samples_per_step = RATE * 15 / tempo;
+    /* duration of one sequencer step, in samples */
+    const int samples_per_step = RATE * 15 / tempo;
 
-    int cycle_samples, half_cycle_samples, quarter_cycle_samples, three_quarter_cycle_samples,
-        lpcycle_samples, lphalf_cycle_samples, lpquarter_cycle_samples, lpthree_quarter_cycle_samples;
+    /* position of each wave in its oscillation */
+    int cycle_pos[2] = {0, 3};
+
+    /* whether the wave is currently playing */
+    char play[2] = {0, 0};
 
     for (int step = 0; step < 16; step++) {
 
-        /* FIXME no-note cells should continue previous wave, not restart it */
+        /* wavelength, in samples */
+        int cycle_samples[2];
 
-        char note = pattern[step].note,
-             lpnote = pattern[step].lpnote;
+        /* important points of the wavelength, in number of samples since the
+         * start of the wave */
+        int end_high[2], begin_low[2], end_low[2];
 
-        if (note) {
-            cycle_samples = samples_per_cycle(note);
-            half_cycle_samples = cycle_samples >> 1;
-            quarter_cycle_samples = half_cycle_samples >> 1; /* FIXME just 50% duty cycle for now */
-            three_quarter_cycle_samples = half_cycle_samples + quarter_cycle_samples;
+        for (int channel = 0; channel < 2; channel++) {
+
+            char note = pattern[step][channel].note;
+            char duty = pattern[step][channel].duty;
+
+            if (note > 0) {
+                play[channel] = 1;
+                cycle_samples[channel] = samples_per_cycle(note);
+
+                /* make math cheaper per sample */
+                begin_low[channel] = cycle_samples[channel] >> 1;
+                end_high[channel] = begin_low[channel] >> duty;
+                end_low[channel] = begin_low[channel] + end_high[channel];
+
+            } else if (note < 0) {
+                play[channel] = 0;
+            }
+            /* else note == 0, so continue oscillating previous note */
         }
 
-        if (lpnote) {
-            lpcycle_samples = samples_per_cycle(lpnote);
-            lphalf_cycle_samples = lpcycle_samples >> 1;
-            lpquarter_cycle_samples = lphalf_cycle_samples >> 1; /* FIXME just 50% duty cycle for now */
-            lpthree_quarter_cycle_samples = lphalf_cycle_samples + lpquarter_cycle_samples;
-        }
-
+        int16_t sample = 0;
         for (int i = 0; i < samples_per_step; i++) {
-            int pos = i % cycle_samples,
-                lppos = i % lpcycle_samples;
+            for (int channel = 0; channel < 2; channel++) {
 
-            int16_t note_level = (pos < quarter_cycle_samples) ? (INT16_MAX >> (1 + MERCY)) :
-                              (pos >= half_cycle_samples && pos < three_quarter_cycle_samples) ? (INT16_MIN >> (1 + MERCY)) :
-                              0;
-            int16_t lpnote_level = (lppos < lpquarter_cycle_samples) ? (INT16_MAX >> (1 + MERCY)) :
-                                (lppos >= lphalf_cycle_samples && lppos < lpthree_quarter_cycle_samples) ? (INT16_MIN >> (1 + MERCY)) :
-                                0;
+                if (!play[channel])
+                    continue;
 
-            int16_t sample = note_level + lpnote_level;
+                int pos = cycle_pos[channel];
+
+                /* first half of the wave */
+                if (pos < begin_low[i]) {
+
+                    /* within the high section */
+                    if (pos < end_high[i]) {
+                        sample += INT16_MAX >> (1 + MERCY);
+                    }
+
+                /* second half of the wave */
+                } else {
+
+                    /* within the low section */
+                    if (pos < end_low[i]) {
+                        sample += INT16_MIN >> (1 + MERCY);
+                    }
+                }
+            }
+
+            /* at this point, we're in two-and-a-half bit space: [-2, -1, 0, 1, 2 ]
+             *
+             *            lpnote_level
+             *           | -1   0   1
+             *         --+------------
+             * note_  -1 | -2  -1   0
+             * level   0 | -1   0   1
+             *         1 |  0   1   2
+             *
+             * where 2 and 1 represent INT16_MAX and INT16_MAX/2, respectively
+             *
+             * the H-bridge + buzzer combo can only drive the speaker forward
+             * (1), backward (-1), or toward a rest state (0). if requested, we
+             * can emulate how that would sound by slamming all positive
+             * numbers up to INT16_MAX and all negative numbers down to
+             * INT16_MIN.
+             */
             if (EMULATE_SHITTY_BADGE_AUDIO)
-                sample = QUANTIZE(note_level + lpnote_level, INT16_MIN >> MERCY, INT16_MAX >> MERCY);
+                sample = QUANTIZE(sample, INT16_MIN >> MERCY, INT16_MAX >> MERCY);
 
             write(audio_pipe, (char *) &sample, sizeof(sample));
         }
@@ -161,21 +269,36 @@ void audio(int audio_pipe)
     close(audio_pipe);
 }
 
-void deconstruct_note(char note, char * const note_name, char * const accidental, char * const octave)
+void tb_put_note(const struct note_t * const note, struct tb_cell * const cell, int x, int y)
 {
-    if (note == 0) {
-        *note_name = *accidental = *octave = '-';
-        return;
+    char note_name, accidental, octave;
+
+    if (note->note == 0) {
+        note_name = accidental = octave = '-';
+    } else if (note->note > NOTE_MAX) {
+        note_name = accidental = octave = '!';
+    } else if (note->note < 0) {
+        note_name = octave = ' ';
+        accidental = 'K';
+    } else {
+        note_name  = "bccddeffggaa"[note->note % 12];
+        accidental = "  # #  # # #"[note->note % 12];
+        octave = "23456789"[(note->note - 1) / 12];
     }
 
-    if (note > 63) {
-        *note_name = *accidental = *octave = '!';
-        return;
-    }
+    cell->ch = note_name;
+    tb_put_cell(x, y, cell);
 
-    *note_name  = "bccddeffggaa"[note % 12];
-    *accidental = "  # #  # # #"[note % 12];
-    *octave = "234567"[(note - 1) / 12];
+    cell->ch = accidental;
+    tb_put_cell(x + 1, y, cell);
+
+    cell->ch = octave;
+    tb_put_cell(x + 2, y, cell);
+
+    tb_puts(duties[(int) note->duty], cell, x + 4, y);
+
+    cell->ch = '%';
+    tb_put_cell(x + 6, y, cell);
 }
 
 void draw_note_columns(enum column_t column)
@@ -190,34 +313,18 @@ void draw_note_columns(enum column_t column)
 
     struct tb_cell *cell;
 
-    char note_name, accidental, octave, lpnote_name, lpaccidental, lpoctave;
     for (int row = 0; row < 16; row++) {
-
-        deconstruct_note(pattern[row].note, &note_name, &accidental, &octave);
-        deconstruct_note(pattern[row].lpnote, &lpnote_name, &lpaccidental, &lpoctave);
 
         cell = (row % 4 == 0) ? &bright : &dark;
 
-        /* regular note column */
-        cell->ch = note_name;
-        tb_put_cell(3, row + 3, cell);
-        cell->ch = accidental;
-        tb_put_cell(4, row + 3, cell);
-        cell->ch = octave;
-        tb_put_cell(5, row + 3, cell);
-
-        /* low-priority column */
-        cell->ch = lpnote_name;
-        tb_put_cell(7, row + 3, cell);
-        cell->ch = lpaccidental;
-        tb_put_cell(8, row + 3, cell);
-        cell->ch = lpoctave;
-        tb_put_cell(9, row + 3, cell);
+        /* note columns */
+        for (int i = 0; i < 2; i++)
+            tb_put_note(&pattern[row][i], cell, 7 * i + 3, row + 3);
 
         /* line number or left arrow */
         const char is_current_line = (row == current_line);
-        const char left_arrow = is_current_line && column == NOTE;
-        const char right_arrow = is_current_line && column == LPNOTE;
+        const char left_arrow = is_current_line && column == LEFT;
+        const char right_arrow = is_current_line && column == RIGHT;
 
         dcell.ch = (left_arrow) ? '-' : "0123456789abcdef"[row];
         tb_put_cell(0, row + 3, &dcell);
@@ -321,7 +428,9 @@ int main(int argc, char *argv[])
 
     struct tb_event event;
 
-    char last_edit = 25;
+    struct note_t last_edit;
+    last_edit.note = 25;
+    last_edit.duty = 1;
 
     enum redraw redraw_setting = FULL;
     enum global_mode_t global_mode = SEQUENCER;
@@ -331,9 +440,9 @@ int main(int argc, char *argv[])
 
     char quit_request = 0;
 
-    enum column_t column = NOTE;
+    enum column_t column = LEFT;
 
-    char *edit_note;
+    struct note_t *edit_note;
 
     for (;;) {
 
@@ -348,7 +457,7 @@ int main(int argc, char *argv[])
 
         if (global_mode == SEQUENCER) {
             draw_note_columns(column);
-            edit_note = (column == NOTE) ? &pattern[current_line].note : &pattern[current_line].lpnote;
+            edit_note = (column == LEFT) ? &pattern[current_line][0] : &pattern[current_line][1];
         }
 
         tb_present();
@@ -424,13 +533,16 @@ int main(int argc, char *argv[])
                 break;
 
             case TB_KEY_TAB:
-                column = (column == NOTE) ? LPNOTE : NOTE;
+                column = (column == LEFT) ? RIGHT : LEFT;
                 break;
 
             case TB_KEY_BACKSPACE2:
             case TB_KEY_DELETE:
-                last_edit = *edit_note;
-                *edit_note = 0;
+                if (edit_note->note > 0) {
+                    last_edit.note = edit_note->note;
+                    last_edit.duty = edit_note->duty;
+                }
+                edit_note->note = 0;
                 break;
             }
         }
@@ -444,7 +556,8 @@ int main(int argc, char *argv[])
 
         case 'C':
             for (int i = 0; i < 16; i++)
-                pattern[i].note = 0;
+                for (int channel = 0; i < 2; i++)
+                    pattern[i][channel].note = 0;
             break;
 
         /* tap tempo */
@@ -469,21 +582,29 @@ int main(int argc, char *argv[])
             break;
 
         case 'H':
-            if (*edit_note == 0)
-                *edit_note = last_edit;
+            if (edit_note->note <= 0) {
+                edit_note->note = last_edit.note;
+                edit_note->duty = last_edit.duty;
+            }
 
-            if (*edit_note < 13)
-                last_edit = *edit_note = 1;
+            if (edit_note->note < 13)
+                last_edit.note = edit_note->note = 1;
             else
-                last_edit = *edit_note -= 12;
+                last_edit.note = edit_note->note -= 12;
+            last_edit.duty = edit_note->duty;
+
             break;
 
         case 'h':
-            if (*edit_note == 0)
-                *edit_note = last_edit;
+            if (edit_note->note <= 0) {
+                edit_note->note = last_edit.note;
+                edit_note->duty = last_edit.duty;
+            }
 
-            if (*edit_note > 1)
-                last_edit = --*edit_note;
+            if (edit_note->note > 1) {
+                last_edit.note = --edit_note->note;
+                last_edit.duty = edit_note->duty;
+            }
             break;
 
         case 'j':
@@ -497,21 +618,33 @@ int main(int argc, char *argv[])
             break;
 
         case 'l':
-            if (*edit_note == 0)
-                *edit_note = last_edit;
+            if (edit_note->note <= 0) {
+                edit_note->note = last_edit.note;
+                edit_note->duty = last_edit.duty;
+            }
 
-            if (*edit_note < 63)
-                last_edit = ++*edit_note;
+            if (edit_note->note < 63) {
+                last_edit.note = ++edit_note->note;
+                last_edit.duty = edit_note->duty;
+            }
             break;
 
         case 'L':
-            if (*edit_note == 0)
-                *edit_note = last_edit;
+            if (edit_note->note <= 0) {
+                edit_note->note = last_edit.note;
+                edit_note->duty = last_edit.duty;
+            }
 
-            if (*edit_note > 51)
-                last_edit = *edit_note = 63;
+            if (edit_note->note > 51)
+                last_edit.note = edit_note->note = 63;
             else
-                last_edit = *edit_note += 12;
+                last_edit.note = edit_note->note += 12;
+            edit_note->duty = last_edit.duty;
+            break;
+
+        case 'X':
+        case 'x':
+            edit_note->note = -1;
             break;
 
         case '=':
@@ -543,7 +676,8 @@ int main(int argc, char *argv[])
             break;
 
         case '.':
-            *edit_note = last_edit;
+            edit_note->note = last_edit.note;
+            edit_note->duty = last_edit.duty;
             break;
 
         case '?':
